36f24d604d09a0a3acc03354c9b80feb37fc61b3
sigh5 <54612487+sigh5@users.noreply.github.com>
2022-10-14 04:22:02 +0900

    ....

--- Client/Code/Logo.cpp
+++ Client/Code/Logo.cpp
@@ -36,7 +36,7 @@ HRESULT CLogo::Ready_Scene(void)
 
 	FAILED_CHECK_RETURN(Ready_Layer_Environment(L"Ready_Layer_Environment"), E_FAIL);
 											// LOADING_STAGE  LOADING_TOOL
-	m_pLoading = CLoading::Create(m_pGraphicDev, LOADING_STAGE);
+	m_pLoading = CLoading::Create(m_pGraphicDev, LOADING_TOOL);
 	NULL_CHECK_RETURN(m_pLoading, E_FAIL);
 		
 	Engine::LoadSoundFile();
@@ -61,7 +61,7 @@ Engine::_int CLogo::Update_Scene(const _float& fTimeDelta)
 	{
 		if (m_pStartButton->Get_Click())
 		{						// CChange_Stage  CToolTest
-			CScene*		pScene = CChange_Stage::Create(m_pGraphicDev);
+			CScene*		pScene = CToolTest::Create(m_pGraphicDev);
 			NULL_CHECK_RETURN(pScene, E_FAIL);
 
 			//m_SceneType = SCENE_TOOLTEST;
