cb72204dbf1ed9f97d783bd2484c0b7e09b522cb
sigh5 <54612487+sigh5@users.noreply.github.com>
2022-10-19 23:40:44 +0900

    Edit

--- Client/Code/FileIOMgr.cpp
+++ Client/Code/FileIOMgr.cpp
@@ -7,7 +7,7 @@
 #include "Anubis.h"
 #include "FatBat.h"
 #include "Spider.h"
-#include "TestPlayer.h"
+
 #include "WallCube.h"
 #include "MonsterToolObject.h"
 #include "Terrain.h"
@@ -422,7 +422,7 @@ void CFileIOMgr::Load_FileData(LPDIRECT3DDEVICE9 pGrahicDev,
 	//ÁÖ¼®Áö¿ì¼À
 	else if (eObjType == OBJ_PLAYER)
 	{
-		ReadFile(hFile, &vRight, sizeof(_vec3), &dwByte, nullptr);
+		/*ReadFile(hFile, &vRight, sizeof(_vec3), &dwByte, nullptr);
 		ReadFile(hFile, &vUp, sizeof(_vec3), &dwByte, nullptr);
 		ReadFile(hFile, &vLook, sizeof(_vec3), &dwByte, nullptr);
 		ReadFile(hFile, &vPos, sizeof(_vec3), &dwByte, nullptr);
@@ -451,7 +451,7 @@ void CFileIOMgr::Load_FileData(LPDIRECT3DDEVICE9 pGrahicDev,
 		Transcom->Set_Angle(&vAngle);
 		Transcom->Set_Scale(&vScale);
 
-		Transcom->Update_Component(0.01f);
+		Transcom->Update_Component(0.01f);*/
 	}
 
 	else if (eObjType == OBJ_ROOM)
